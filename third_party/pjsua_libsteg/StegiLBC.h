0b514093b6bd3cb33a50b3a5549cbdf3ead73684
BobMu <1224357941@qq.com>
2018-05-15 14:16:12 +0800

    change var

--- third_party/pjsua_libsteg/StegiLBC.h
+++ third_party/pjsua_libsteg/StegiLBC.h
@@ -3,11 +3,11 @@
 //THZ: 将三帧的容量改为一帧
 #define iLBC_SAEDU_20      9   //三帧的隐藏容量
 #define iLBC_SAEDU_30      11 
-#define g711_SAEDU         9  //g711算法中的隐藏容量
+#define g711_SAEDU         20  //g711算法中的隐藏容量
 
 #define iLBC_FRAME_LENTH_20		38
 #define iLBC_FRAME_LENTH_30		50
-#define g711_FRAME_LENTH        38  //g711算法下的帧长度
+#define g711_FRAME_LENTH        40  //g711算法下的帧长度
 
 #define iLBC_VOICE_LENTH_20		320
 #define iLBC_VOICE_LENTH_30		480
