66635e2cb37f3580e0c77ff1ec989685df442657
BobMu <1224357941@qq.com>
2018-05-21 12:55:36 +0800

    clock rate 32000
    Signed-off-by: BobMu <1224357941@qq.com>

--- pjsip/include/pjsua-lib/pjsua.h
+++ pjsip/include/pjsua-lib/pjsua.h
@@ -5490,7 +5490,7 @@ PJ_DECL(pj_status_t) pjsua_im_typing(pjsua_acc_id acc_id,
  */
 #ifndef PJSUA_DEFAULT_CLOCK_RATE
 //#   define PJSUA_DEFAULT_CLOCK_RATE	16000
-#   define PJSUA_DEFAULT_CLOCK_RATE	16000*3
+#   define PJSUA_DEFAULT_CLOCK_RATE	16000*2
 #endif
 
 /**
@@ -5499,7 +5499,7 @@ PJ_DECL(pj_status_t) pjsua_im_typing(pjsua_acc_id acc_id,
  */
 #ifndef PJSUA_DEFAULT_AUDIO_FRAME_PTIME
 //#   define PJSUA_DEFAULT_AUDIO_FRAME_PTIME  20
-#   define PJSUA_DEFAULT_AUDIO_FRAME_PTIME  20*3
+#   define PJSUA_DEFAULT_AUDIO_FRAME_PTIME  20*1
 #endif
 
 
